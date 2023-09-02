760b2ccd12f3fda008ea43412ac4e1c2359b7886
jenny-chung <jennychung16@gmail.com>
2023-09-02 02:42:16 -0700

    extend token age

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -10,7 +10,7 @@ const generateToken = (res, userID) => {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
         sameSite: 'strict',
-        maxAge: 1 * 24 * 60 * 60 * 1000 // 1 day
+        maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
     });
 };
 
