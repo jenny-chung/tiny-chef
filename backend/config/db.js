2fb9ba50c84037f3794c519ef66ce3f0e9517015
jenny-chung <jennychung16@gmail.com>
2023-09-10 02:20:30 -0700

    update message for db connection error

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`Error: ${error.message}`);
+        console.error(`Error occurred while connecting to MongoDB: ${error.message}`);
         process.exit(1);
     }
 };
