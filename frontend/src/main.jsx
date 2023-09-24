9a1cfe0312d07c8ab4e4601fd4b38509804049a3
jenny-chung <jennychung16@gmail.com>
2023-09-23 18:06:03 -0700

    change to use browser router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { 
-  createHashRouter,
+  createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider
@@ -21,7 +21,7 @@ import UpdateRecipe from './components/UpdateRecipe.jsx';
 import RecipeDisplay from './screens/RecipeDisplay.jsx';
 import SavedRecipes from './screens/SavedRecipes.jsx';
 
-const router = createHashRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
